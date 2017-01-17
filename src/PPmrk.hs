017d646f9d5d8594860686c5b4faecf781d7a886
oleks <oleks@oleks.info>
2017-01-17 12:42:15 +0100

    Add type declarations

--- src/PPmrk.hs
+++ src/PPmrk.hs
@@ -7,6 +7,7 @@ import Text.PrettyPrint
 toMrk :: [Judgement] -> String
 toMrk js = render $ vcat $ map (formatJudgement 1) js
 
+formatJudgement :: Int -> Judgement -> Doc
 formatJudgement level (Judgement (header, comments, judgements)) =
   formatHeader level header $+$
   (nest 2 $ vcat $ map (formatComment) comments) $+$
@@ -19,17 +20,21 @@ pointsDoc :: Double -> Doc
 pointsDoc v | isIntegral v = integer (round v)
 pointsDoc v = double v
 
+formatHeader :: Int -> Header -> Doc
 formatHeader level (Header (title, point, maxPoints)) =
   (text $ replicate level '#') <+> text title <> colon <+>
     pointsDoc point <> text "/" <> pointsDoc maxPoints
 
+formatComment :: Comment -> Doc
 formatComment (Comment (mood, commentParts)) =
   formatMood mood <+> (vcat $ map formatCommentPart commentParts)
 
+formatMood :: Mood -> Doc
 formatMood Positive  = text "+"
 formatMood Negative  = text "-"
 formatMood Neutral   = text "*"
 formatMood Impartial = text "?"
 
+formatCommentPart :: CommentPart -> Doc
 formatCommentPart (CommentStr string)  = text string
 formatCommentPart (CommentCmt comment) = nest 2 $ formatComment comment
