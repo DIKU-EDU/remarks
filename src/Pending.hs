35f8463d2c9cb17a54026c45f599b491d3758f01
Michael Kirkedal Thomsen <kirkedal@acm.org>
2017-01-23 15:39:19 +0100

    Listing impartial comments as part of pending

--- src/Pending.hs
+++ src/Pending.hs
@@ -4,10 +4,10 @@ module Pending ( findPending ) where
 
 import Ast
 
-import Data.Tree
 import Text.PrettyPrint
 
-type PendingTree = Tree String
+data PendingTree
+  = Node String Int [PendingTree]
 
 data FormatTree
   = TEmpty  FormatTree
@@ -15,6 +15,9 @@ data FormatTree
   | TBranch FormatTree
   | TNode
   | TLeaf
+  | TQuest
+  | TQFollow
+  | TQNone
 
 showTree :: FormatTree -> Doc
 showTree (TEmpty t)  = showTree t
@@ -22,53 +25,92 @@ showTree (TBranch t) = text " | " <> showTree t
 showTree (TSpace t)  = text "   " <> showTree t
 showTree TNode       = text " |- "
 showTree TLeaf       = text " |> "
+showTree TQuest      = text " |? "
+showTree TQFollow    = text " |  "
+showTree TQNone      = text "    "
 
 linebreak :: Doc
 linebreak = text "\n"
+
+isLeaf :: PendingTree -> Bool
+isLeaf (Node _ _ [])    = True
+isLeaf (Node _ _ (_:_)) = False
 
 formatTree :: PendingTree -> Doc
 formatTree t = formatSubTree (TEmpty) t
 
 formatSubTrees :: (FormatTree -> FormatTree) -> [PendingTree] -> Doc
 formatSubTrees _ [] = empty
-formatSubTrees ft [(Node s [])] =
-  showTree (ft TLeaf) <> text s
+formatSubTrees ft [t] | isLeaf t =
+  linebreak <> showTree (ft TLeaf) <> formatSubTree (ft . TSpace) t
 formatSubTrees ft [t] =
-  showTree (ft TNode) <> formatSubTree (ft . TSpace) t
-formatSubTrees ft ((Node s []):ts) =
-  showTree (ft TLeaf) <> text s <> linebreak <>
-  (formatSubTrees ft ts)
+  linebreak <> showTree (ft TNode) <> formatSubTree (ft . TSpace) t
+formatSubTrees ft (t:ts) | isLeaf t =
+  linebreak <> showTree (ft TLeaf) <> formatSubTree (ft . TBranch) t <>
+  formatSubTrees ft ts
 formatSubTrees ft (t:ts) =
-  showTree (ft TNode) <> formatSubTree (ft . TBranch) t <> linebreak <>
-  (formatSubTrees ft ts)
+  linebreak <> showTree (ft TNode) <> formatSubTree (ft . TBranch) t <>
+  formatSubTrees ft ts
 
 formatSubTree :: (FormatTree -> FormatTree) -> PendingTree -> Doc
-formatSubTree _ (Node s []) =
-  text s
-formatSubTree tree (Node s ts) =
-  text s <> linebreak <> formatSubTrees tree ts
+-- formatSubTree ft (Node s cs []) = text s <> formatTreeComments ft cs
+formatSubTree ft (Node s cs ts) = text s <> formatTreeComments ft cs <> formatSubTrees ft ts
 
-size :: PendingTree -> Int
-size (Node _ []) = 1
-size (Node _ (t @ (_:_))) = sum $ map size t
+formatTreeComments :: (FormatTree -> FormatTree) -> Int -> Doc
+formatTreeComments _ 0 = empty
+formatTreeComments ft cs =
+  linebreak <> showTree (ft TQuest) <> text (makePlural cs "impartial comment")
+
+size :: PendingTree -> (Int, Int)
+size (Node _ 0 []) = (1, 0)
+size (Node _ cs []) = (1, cs)
+size (Node _ cs (t @ (_:_))) = (sum $ fst sizes, cs + (sum $ snd sizes))
+  where sizes = unzip $ map size t
 
 limitPendingTree :: Int -> PendingTree -> PendingTree
-limitPendingTree _ (Node t []) = (Node t [])
-limitPendingTree 0 (Node t sub) = (Node (t ++ showTasks (sum $ map size sub)) [])
-limitPendingTree n (Node t sub) = (Node t (map (limitPendingTree (n-1)) sub))
+limitPendingTree _    (Node s 0 []) = Node s 0 []
+limitPendingTree 0 (t@(Node s _ _))  = Node (s ++ showTasks (size t)) 0 []
+limitPendingTree n    (Node s cs ts) = Node s cs (map (limitPendingTree (n-1)) ts)
 
-showTasks :: Int -> String
-showTasks 1 = " (1 task)"
-showTasks n = " (" ++ (show n) ++ " tasks)"
+showTasks :: (Int, Int) -> String
+showTasks (n, 0) = " (" ++ makePlural n "task" ++ ")"
+showTasks (0, m) = " (" ++ makePlural m "comment" ++ ")"
+showTasks (n, m) = " (" ++ makePlural n "task" ++ " and " ++ makePlural m "comment" ++ ")"
+
+makePlural :: Int -> String -> String
+makePlural 0 s = "0 " ++ s ++ "s"
+makePlural 1 s = "1 " ++ s
+makePlural n s = show n ++ " " ++ s ++ "s"
 
 pendingJudgement :: Judgement -> [PendingTree]
-pendingJudgement (Bonus (_, _)) = []
-pendingJudgement (Judgement (Header (t, p, _), _, _, [])) | isInfinite p = [Node t []]
-pendingJudgement (Judgement (Header (_, _, _), _, _, [])) = [] -- Not infinite
-pendingJudgement (Judgement (Header (t, _, _), _, _, subJs @ (_:_))) =
+pendingJudgement (Bonus (_, cs)) =
+  case countImpartials cs of
+    0 -> []
+    n -> [Node "Bonus" n []]
+pendingJudgement (Judgement (Header (t, p, _), _, cs, [])) | isInfinite p = 
+  [Node t (countImpartials cs) []]
+pendingJudgement (Judgement (Header (t, _, _), _, cs, [])) = -- Not infinite
+  case countImpartials cs of
+    0 -> []
+    n -> [Node t n []]
+pendingJudgement (Judgement (Header (t, _, _), _, cs, subJs @ (_:_))) =
   case (concatMap pendingJudgement subJs) of
-    [] -> []
-    sub  -> [Node t sub]
+    [] -> 
+      case countImpartials cs of
+        0 -> []
+        n -> [Node t n []]
+    sub  -> [Node t (countImpartials cs) sub]
+
+countImpartials :: [Comment] -> Int
+countImpartials = sum . (map countImpartial)
+
+countImpartial :: Comment -> Int
+countImpartial (Comment (Impartial, cps)) = 1 + (sum $ map countImpartialCP cps)
+countImpartial (Comment (_, cps)) = sum $ map countImpartialCP cps
+
+countImpartialCP :: CommentPart -> Int
+countImpartialCP (CommentStr _) = 0
+countImpartialCP (CommentCmt c) = countImpartial c
 
 findPending :: Maybe Int -> [Judgement] -> Maybe(String)
 findPending detailLevel js =
