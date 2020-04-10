148ca6bfceff1529eb3ffa5aa8309c3431fb854f
Oleks <oleks@oleks.info>
2020-04-10 16:54:14 +0200

    Fix pretty-printing multiple feedback judgements
    Addresses question 1 in #28

--- src/Export/Feedback.hs
+++ src/Export/Feedback.hs
@@ -7,7 +7,7 @@ import Prelude hiding ((<>))
 import Text.PrettyPrint
 
 feedbackRemarks :: [Judgement] -> String
-feedbackRemarks = render . hcat . map (formatJudgement 1)
+feedbackRemarks = render . vcat . map (formatJudgement 1)
 
 formatJudgement :: Int -> Judgement -> Doc
 formatJudgement _ (Feedback (_, t)) = text t
@@ -15,7 +15,7 @@ formatJudgement _ (Bonus (_, _, _)) = empty
 formatJudgement depth (j @ (Judgement (_, _, _, judgements))) =
   case isEmpty subj of
     True -> empty
-    False -> formatHeader depth j $+$ text "" $+$ subj
+    False -> formatHeader depth j $+$ text "" $+$ subj $+$ text ""
   where
     subj = vcat $ map (formatJudgement (depth + 1)) judgements
 
