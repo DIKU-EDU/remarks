2cbe5220a227821788951b314f7d85950e4cf42d
Troels Henriksen <athas@sigkill.dk>
2022-01-31 17:18:18 +0100

    Remove whitespace.

--- src/Export/SimpleTable.hs
+++ src/Export/SimpleTable.hs
@@ -11,32 +11,32 @@ import Text.PrettyPrint
 import Data.List (intersperse)
 
 
-simpleTableRemarks :: [Judgement] -> Table 
+simpleTableRemarks :: [Judgement] -> Table
 simpleTableRemarks js = Rows $ tblHead:(map formatStart js)
   where
     tblHead = "Student":"Total / 100":(tail $ tableHead (head js))
 
 tableHead :: Judgement -> Row
-tableHead (j @ (Bonus _)) = [getTitle j]
+tableHead (j@(Bonus _)) = [getTitle j]
 tableHead (Feedback _) = []
-tableHead (j @ (Judgement (_, _, _, js))) =
+tableHead (j@(Judgement (_, _, _, js))) =
   ((getTitle j) ++ " / " ++ (getMaxPoints j)):(concatMap tableHead js)
 
 formatStart :: Judgement -> Row
 formatStart (Bonus _) = []
 formatStart (Feedback _) = []
-formatStart (j @ (Judgement (_, _, cs, js))) =
+formatStart (j@(Judgement (_, _, cs, js))) =
   (getTitle j):(getTotal j):r1
   -- [(getTitle j):(getTotal j):r1]
   where
     r1 = concatMap formatJudgement js
 
 formatJudgement :: Judgement -> Row
-formatJudgement (j @ (Bonus (_, _, cs))) = [getTotal j]
+formatJudgement (j@(Bonus (_, _, cs))) = [getTotal j]
 formatJudgement (Feedback _) = []
 -- formatJudgement (j @ (Judgement (Header(_,p,_), _, cs, []))) =
 --   [getTotal j]
-formatJudgement (j @ (Judgement (_, _, cs, js))) =
+formatJudgement (j@(Judgement (_, _, cs, js))) =
   (getTotal j):r1
   where
     r1 = concatMap formatJudgement js
