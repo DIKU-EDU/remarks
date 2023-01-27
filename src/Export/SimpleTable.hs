e1aa15dbb1981cf82fc5f365e344a6acef51305d
Oleks <oleks@oleks.info>
2023-01-28 00:01:35 +0100

    Use _ for unused parameters

--- src/Export/SimpleTable.hs
+++ src/Export/SimpleTable.hs
@@ -21,18 +21,18 @@ tableHead (j@(Judgement (_, _, _, js))) =
 formatStart :: Judgement -> Row
 formatStart (Bonus _) = []
 formatStart (Feedback _) = []
-formatStart (j@(Judgement (_, _, cs, js))) =
+formatStart (j@(Judgement (_, _, _, js))) =
   (getTitle j):(getTotal j):r1
   -- [(getTitle j):(getTotal j):r1]
   where
     r1 = concatMap formatJudgement js
 
 formatJudgement :: Judgement -> Row
-formatJudgement (j@(Bonus (_, _, cs))) = [getTotal j]
+formatJudgement (j@(Bonus (_, _, _))) = [getTotal j]
 formatJudgement (Feedback _) = []
 -- formatJudgement (j @ (Judgement (Header(_,p,_), _, cs, []))) =
 --   [getTotal j]
-formatJudgement (j@(Judgement (_, _, cs, js))) =
+formatJudgement (j@(Judgement (_, _, _, js))) =
   (getTotal j):r1
   where
     r1 = concatMap formatJudgement js
